cf5a7b6ae7a84cbf25a45733616555295dac6e39
limadouglas07 <lima.douglas08@gmail.com>
2025-02-18 20:02:03 -0300

    Update restaurente.py

--- modelo/restaurente.py
+++ modelo/restaurente.py
@@ -1,5 +1,5 @@
 class Restaurante:
-    def __init__(self, nome, categoria): # onde está "eu" considerar como Self.
+    def __init__(self, nome, categoria):
         self.nome = nome 
         self.categoria = categoria
         self.ativo = False
