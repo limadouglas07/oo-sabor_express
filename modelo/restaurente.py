9de8d75b5e66120a50caf3a618d9046fc9a91b07
limadouglas07 <lima.douglas08@gmail.com>
2025-02-18 19:51:58 -0300

    Update restaurente.py

---
+++ modelo/restaurente.py
@@ -0,0 +1,13 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+restaurante_praca = Restaurante('Praça','Gourmet')
+restaurante_pizza = Restaurante('Pizza express', 'Italiana')
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
